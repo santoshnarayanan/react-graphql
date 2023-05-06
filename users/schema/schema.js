e4a69a0689931e043421c322c529dba8c586a3f4
Santosh Narayanan <santosh33@gmail.com>
2023-05-06 23:43:43 +0200

    Define RootQuery

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -3,11 +3,26 @@ const {
     GraphQLObjectType, GraphQLString, GraphQLInt
 } = graphql;
 
+//Define the user type
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt }
+    }
+});
+
+//Define the root query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                //fetch actual data from the database
+            }
+        }
     }
 });
