fc3056f312e7841db35e699c9c1263029853cf6d
Santosh Narayanan <santosh33@gmail.com>
2023-05-06 23:21:04 +0200

    Updated code

--- users/server.js
+++ users/server.js
@@ -1,6 +1,12 @@
-const { createHandler } = require("graphql-http/lib/use/express");
+//const { createHandler } = require("graphql-http/lib/use/express");
+const expressGraphQL = require('express-graphql');
 const express = require("express");
+
 const app = express();
-app.use("/graphql", createHandler({ graphql: true, }));
+
+//Registering the GraphQL express server
+//app.use("/graphql", createHandler({ graphql: true, }));
+
+app.use("/graphql", expressGraphQL.graphqlHTTP({ graphiql: true, }));
 app.listen({ port: 4000 });
 console.log("Listening to port 4000");
