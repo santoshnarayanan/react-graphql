fce4a91c67456bc8068dd9e766676d03ec3c2c1c
Santosh Narayanan <santosh33@gmail.com>
2023-05-06 23:54:16 +0200

    Resolving Data

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,7 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
-    GraphQLObjectType, GraphQLString, GraphQLInt
+    GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema
 } = graphql;
+
+//Create data with hard coded values
+const users = [
+    { id: '23', firstName: 'Bill', age: 30 },
+    { id: '47', firstName: 'Samantha', age: 27 },
+]
 
 //Define the user type
 const UserType = new GraphQLObjectType({
@@ -22,7 +29,13 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
                 //fetch actual data from the database
+                return _.find(users, { id: args.id });
             }
         }
     }
+});
+
+//make it available to other
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
--- users/server.js
+++ users/server.js
@@ -1,12 +1,13 @@
 //const { createHandler } = require("graphql-http/lib/use/express");
 const expressGraphQL = require('express-graphql');
 const express = require("express");
+const schema = require("./schema/schema");
 
 const app = express();
 
 //Registering the GraphQL express server
 //app.use("/graphql", createHandler({ graphql: true, }));
 
-app.use("/graphql", expressGraphQL.graphqlHTTP({ graphiql: true, }));
+app.use("/graphql", expressGraphQL.graphqlHTTP({ schema: schema, graphiql: true, }));
 app.listen({ port: 4000 });
 console.log("Listening to port 4000");
