e8449ba32aa3e0bed3d287379286feac25e66ba3
Santosh Narayanan <santosh33@gmail.com>
2023-05-07 20:14:27 +0200

    Edit Mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -81,10 +81,24 @@ const mutation = new GraphQLObjectType({
         },
         deleteUser: {
             type: UserType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: new GraphQLNonNull(GraphQLString) } },
             resolve(parentValue, args) {
                 //delete actual data from the json-server hosted on localhost:3000
                 return axios.delete(`http://localhost:3000/users/${args.id}`).then(res => res.data);
+            }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                //delete actual data from the json-server hosted on localhost:3000
+                return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                    .then(res => res.data);
             }
         }
     }
