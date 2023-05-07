4aa11ac0bd68a9d735a06035a302742deadf9f04
Santosh Narayanan <santosh33@gmail.com>
2023-05-07 17:15:52 +0200

    Bi-directional query

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,16 +1,25 @@
 const graphql = require('graphql');
 const axios = require('axios');
 const {
-    GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema
+    GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList
 } = graphql;
 
-//define the company type above UserType
+//Bi-Directional GraphQL
+//based on http://localhost:3000/companies/2/users
+//get list of users associated to a particular company
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
     fields: {
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(res => res.data);
+            }
+        }
     }
 });
 
